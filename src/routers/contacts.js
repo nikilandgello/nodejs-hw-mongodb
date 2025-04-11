bb12821d92a16fd8e08886ee0aba883a3757fa88
nikilandgello <nikoscha22012004@gmail.com>
2025-04-11 10:47:53 +0300

    fix: add validation id for DELETE

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -34,6 +34,10 @@ router.patch(
   ctrlWrapper(patchContactController),
 );
 
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.delete(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(deleteContactController),
+);
 
 export default router;
